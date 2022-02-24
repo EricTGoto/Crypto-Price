b47e728f512d1220bbd5eb514fc95327aca6fb38
EricTGoto <erictomokigoto@gmail.com>
2022-02-23 21:00:16 -0800

    implemented template inheritance

--- crypto_price/crypto_email/urls.py
+++ crypto_price/crypto_email/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='crypto_email_index'),
+    path('about/', views.about, name='crpyto_email_about'),
 ]
--- crypto_price/crypto_email/views.py
+++ crypto_price/crypto_email/views.py
@@ -3,10 +3,20 @@ from django.http import HttpResponse
 from . import crypto_information
 # Create your views here.
 
+data = [
+    {
+        'name': "Ethereum",
+        'price': "10000"
+    }
+]
+
 def index(request):
-    crypto_info = crypto_information.GetCryptoData()
-    raw_data = crypto_info.getData()
-    data = crypto_info.extractInfo(raw_data, '1027')
-    print(data)
+    #crypto_info = crypto_information.GetCryptoData()
+    #raw_data = crypto_info.getData()
+    #data = crypto_info.extractInfo(raw_data, '1027')
+    #print(data)
     context = { 'data': data }
     return render(request, 'crypto_email/index.html', context)
+
+def about(request):
+    return render(request, 'crypto_email/about.html', {'title': 'About'})
