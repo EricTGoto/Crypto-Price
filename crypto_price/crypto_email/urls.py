d104be81dcd0fd93eb2222fa036881139ad4c31c
EricTGoto <erictomokigoto@gmail.com>
2022-02-23 21:51:14 -0800

    changed hard coded routes for home and about to use {% url %} tag

--- crypto_price/crypto_email/urls.py
+++ crypto_price/crypto_email/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='crypto_email_index'),
-    path('about/', views.about, name='crpyto_email_about'),
+    path('about/', views.about, name='crypto_email_about'),
 ]
--- crypto_price/crypto_price/urls.py
+++ crypto_price/crypto_price/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('crypto_email/', include('crypto_email.urls')),
+    path('', include('crypto_email.urls')),
     path('admin/', admin.site.urls),
 ]
