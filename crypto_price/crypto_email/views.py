83abc4cbfe5c5c335066bf7b78832bbec9d509fa
EricTGoto <erictomokigoto@gmail.com>
2022-02-28 22:10:41 -0800

    updated index view to use data fetching logic

--- crypto_price/crypto_email/views.py
+++ crypto_price/crypto_email/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import crypto_information
+from .models import Top5Crypto
+from django.utils import timezone
 # Create your views here.
 
 data = [
@@ -27,14 +29,7 @@ data = [
 ]
 
 def index(request):
-    crypto_info = crypto_information.GetCryptoData()
-    map_data = crypto_info.get_top_coins(5)
-    data = crypto_info.clean_map_response(map_data)
-    ids = crypto_info.get_IDs(map_data)
-    quotes= crypto_info.get_data(ids)
-    data = crypto_info.add_coin_data_from_quotes_latest(quotes, data)
-    print(data)
-    
+    data = Top5Crypto.fetch_data()
     context = { 'data': data }
     return render(request, 'crypto_email/index.html', context)
 
