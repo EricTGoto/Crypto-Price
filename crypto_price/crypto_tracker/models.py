c934e2aa39c39bf28b6f2fabcf06fac576b090fa
EricTGoto <erictomokigoto@gmail.com>
2022-03-13 13:33:08 -0700

    moved crypto tracker logic to model

--- crypto_price/crypto_tracker/models.py
+++ crypto_price/crypto_tracker/models.py
@@ -1,14 +1,46 @@
 from django.db import models
+from crypto_email.crypto_information import GetCryptoData
 
 class TrackedCoinGroup(models.Model):
     group_name = models.CharField(max_length=10)
 
+    @classmethod
+    def update_view(cls, request):
+        group = request.POST['group']
+
+        #check if group exists, if not make a new group
+        if TrackedCoinGroup.objects.all().filter(group_name=group).count() != 0:
+            # group exits so just add it to the group
+            group = TrackedCoinGroup.objects.get(group_name=group)
+        else:
+            # group doesn't exist so create a new group and add it to the group
+            group = TrackedCoinGroup(group_name=group)
+            group.save()
+        
+        price = TrackedCoin.get_coin_price(request)
+        difference = price - int(request.POST['tracked_price'])
+        
+
+        new_tracked_crypto = group.trackedcoin_set.create(symbol=request.POST['symbol'], tracked_price = request.POST['tracked_price'], price = price, difference = difference)
+        
+        context = {'data': TrackedCoinGroup.objects.all()}
+
+        return context
 class TrackedCoin(models.Model):
     symbol = models.CharField(max_length=5)
     tracked_price = models.DecimalField(max_digits=10, decimal_places=2)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     difference = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     trackedcoingroup = models.ForeignKey(TrackedCoinGroup, on_delete=models.CASCADE)
+
+    @classmethod
+    def get_coin_price(cls, request):
+        crypto = GetCryptoData()
+
+        crypto_price = crypto.get_data_with_symbol(request.POST['symbol'])
+        price = crypto_price['data'][f'{request.POST["symbol"]}']['quote']['USD']['price']
+
+        return price
 
     
 
--- crypto_price/crypto_tracker/views.py
+++ crypto_price/crypto_tracker/views.py
@@ -8,32 +8,9 @@ from crypto_email.crypto_information import GetCryptoData
 def tracker(request):
     crypto = GetCryptoData()
     if request.method == 'POST':
-
-        print("POST")
-        print(request.POST)
-        crypto_price = crypto.get_data_with_symbol(request.POST['symbol'])
-        price = crypto_price['data'][f'{request.POST["symbol"]}']['quote']['USD']['price']
-
-        # difference is positive if current price is greater than tracked price
-        difference = price - int(request.POST['tracked_price'])
-        group = request.POST['group']
-        
-        #check if group exists, if not make a new group
-        if TrackedCoinGroup.objects.all().filter(group_name=group).count() != 0:
-            # group exits so just add it to the group
-            group = TrackedCoinGroup.objects.get(group_name=group)
-        else:
-            # group doesn't exist so create a new group and add it to the group
-            group = TrackedCoinGroup(group_name=group)
-            group.save()
-
-        new_tracked_crypto = group.trackedcoin_set.create(symbol=request.POST['symbol'], tracked_price = request.POST['tracked_price'], price = price, difference = difference)
-        
-        context = {'data': TrackedCoinGroup.objects.all()}
-        print(context)
-        
+        context = TrackedCoinGroup.update_view(request)
         return HttpResponseRedirect(reverse('crypto_tracker:tracker'), context)
-    
+
     context = {'data': TrackedCoinGroup.objects.all()}
     return render(request, 'crypto_tracker/tracker.html', context)
 
