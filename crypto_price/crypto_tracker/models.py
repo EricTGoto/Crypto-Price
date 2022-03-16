059e6b4a919c02887d01fe2e5a167a989c00acfb
EricTGoto <erictomokigoto@gmail.com>
2022-03-16 08:22:12 -0700

    began working on linking together coin, trackedcoin, and trackedcoingroup

--- crypto_price/crypto_tracker/models.py
+++ crypto_price/crypto_tracker/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from crypto_email.crypto_information import GetCryptoData
 
+class Coin(models.Model):
+    symbol = models.CharField(max_length=5)
+    name = models.CharField(max_length=20)
+
 class TrackedCoinGroup(models.Model):
     group_name = models.CharField(max_length=10)
 
@@ -10,6 +14,12 @@ class TrackedCoinGroup(models.Model):
         group = request.POST['group']
         symbol = request.POST['symbol']
 
+        coin_info = crypto_data.get_data_with_symbol(symbol)
+
+        #check if coin exists
+        # if coin doesn't exist, create and return new info
+        if Coin.objects.filter(name=coin_info['name']).count() == 0:
+            Coin(symbol=coin_info['symbol'],name=coin_info['name'])
         #check if group exists, if not make a new group
         if TrackedCoinGroup.objects.all().filter(group_name=group).count() != 0:
             # group exits so just add it to the group
@@ -19,7 +29,6 @@ class TrackedCoinGroup(models.Model):
             group = TrackedCoinGroup(group_name=group)
             group.save()
         
-        coin_info = crypto_data.get_data_with_symbol(symbol)
         difference = coin_info['price'] - int(request.POST['tracked_price'])
 
         new_tracked_crypto = group.trackedcoin_set.create(symbol=request.POST['symbol'], tracked_price = request.POST['tracked_price'], price = coin_info['price'], difference = difference, name=coin_info['name'])
@@ -27,15 +36,6 @@ class TrackedCoinGroup(models.Model):
         context = {'data': TrackedCoinGroup.objects.all()}
 
         return context
-
-class Coin(models.Model):
-    symbol = models.CharField(max_length=5)
-    name = models.CharField(max_length=20)
-
-    # @classmethod
-    # def create_coin(cls, name: str):
-    #     if Coin.objects.filter(name=name).count() == 0:
-    #         Coin(symbol)
 
 class TrackedCoin(models.Model):
     name = models.CharField(max_length=20)
