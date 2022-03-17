0d00cdc1d96e67e44dd369d220bf4ca15e3f65c2
EricTGoto <erictomokigoto@gmail.com>
2022-03-16 20:51:34 -0700

    finished logic to create the many to one relationship of trackedcoin to coin and trackedcoingroup

--- crypto_price/crypto_tracker/models.py
+++ crypto_price/crypto_tracker/models.py
@@ -14,25 +14,30 @@ class TrackedCoinGroup(models.Model):
         group = request.POST['group']
         symbol = request.POST['symbol']
 
-        coin_info = crypto_data.get_data_with_symbol(symbol)
-
-        #check if coin exists
-        # if coin doesn't exist, create and return new info
-        if Coin.objects.filter(name=coin_info['name']).count() == 0:
-            Coin(symbol=coin_info['symbol'],name=coin_info['name'])
         #check if group exists, if not make a new group
         if TrackedCoinGroup.objects.all().filter(group_name=group).count() != 0:
             # group exits so just add it to the group
             group = TrackedCoinGroup.objects.get(group_name=group)
         else:
-            # group doesn't exist so create a new group and add it to the group
+            # group doesn't exist so create a new group
             group = TrackedCoinGroup(group_name=group)
             group.save()
-        
-        difference = coin_info['price'] - int(request.POST['tracked_price'])
 
-        new_tracked_crypto = group.trackedcoin_set.create(symbol=request.POST['symbol'], tracked_price = request.POST['tracked_price'], price = coin_info['price'], difference = difference, name=coin_info['name'])
-        
+        coin_info = crypto_data.get_data_with_symbol(symbol)
+        #check if coin exists
+        # if coin doesn't exist, create and return new info
+        # TODO: return new info
+        if Coin.objects.filter(name=coin_info['name']).count() == 0:
+            coin_info = crypto_data.get_data_with_symbol(symbol)
+            coin = Coin(symbol=coin_info['symbol'], name=coin_info['name'])
+            coin.save()
+        else:
+            # coin exists, so we create a new trackedcoin under the appropriate coin and trackedcoingroup
+            # TODO: return already existing info
+            coin = Coin.objects.get(name=coin_info['name'])
+            
+        difference = coin_info['price'] - int(request.POST['tracked_price'])    
+        coin.trackedcoin_set.create(symbol=request.POST['symbol'], tracked_price = request.POST['tracked_price'], price = coin_info['price'], difference = difference, name=coin_info['name'], trackedcoingroup = group)
         context = {'data': TrackedCoinGroup.objects.all()}
 
         return context
@@ -44,7 +49,7 @@ class TrackedCoin(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     difference = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     trackedcoingroup = models.ForeignKey(TrackedCoinGroup, on_delete=models.CASCADE)
-    #coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
 
 
 
