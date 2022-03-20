b5538744ce43952dadb70ebf23e95045412fcf97
EricTGoto <erictomokigoto@gmail.com>
2022-03-20 11:24:19 -0700

    removed some whitespace and comments

--- crypto_price/crypto_tracker/models.py
+++ crypto_price/crypto_tracker/models.py
@@ -14,7 +14,6 @@ class TrackedCoinGroup(models.Model):
         crypto_data = GetCryptoData()
         group = request.POST['group']
         symbol = request.POST['symbol']
-
         #check if group exists, if not make a new group
         if TrackedCoinGroup.objects.all().filter(group_name=group).count() != 0:
             # group exits so just add it to the group
@@ -37,13 +36,11 @@ class TrackedCoinGroup(models.Model):
             crypto_data.download_image(icon_link)
         else:
             # coin exists, so we create a new trackedcoin under the appropriate coin and trackedcoingroup
-            # TODO: return already existing info
             coin = Coin.objects.get(name=coin_info['name'])
             
         difference = coin.price - int(request.POST['tracked_price'])    
         coin.trackedcoin_set.create(symbol=request.POST['symbol'], tracked_price = request.POST['tracked_price'], price = coin.price,difference = difference, name=coin_info['name'], trackedcoingroup = group)
         context = {'data': TrackedCoinGroup.objects.all()}
-
         return context
 
 class TrackedCoin(models.Model):
